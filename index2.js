aa6ed065b29d846cf36988eece62fe43a6a6677f
kremerdesign <kremerdesign@gmail.com>
2014-07-18 15:42:56 -0700

    updates and using objects

--- index2.js
+++ index2.js
@@ -1,26 +1,140 @@
 $(document).ready(function() {
 
 
+
+var product1 = {
+        'name': "Dude's Rug",
+        'price': 339.99,
+        'desc': "Hey! This is a private residence, man! Yeah man. Well, you know, the Dude abides. Hardly Dude, a new 'vette? The kid's still got, oh, 96 to 97 thousand, depending on the options. The Dude abides.",
+        'featured': true,
+        'product_type': "product",
+        'id': '00001',
+        'discount': 15,
+        'img': 'images/rug-1.jpg'
+    };
+
+var product2 = {
+        'name': "A New Rug",
+        'price': 249.99,
+        'desc': "That, or Duder. His Dudeness. Or El Duderino, if you know, you're not into the whole brevity thing. We wanted to talk about little Larry. May we come in? DO YOU SEE WHAT HAPPENS, LARRY? They call Los Angeles the City of Angels. I didn't find it to be that exactly, but I'll allow as there are some nice folks there.",
+        'featured': true,
+        'product_type': "product",
+        'id': '00002',
+        'discount': 10,
+        'img': 'images/rug-2.jpg'
+    };
+
+var product3 = {
+        'name': "Another New Rug",
+        'price': 289.99,
+        'desc': "Forget it, Donny. You're out of your element. Our basic freedoms. This is quite a pad you got here, man. Completely unspoiled. What's a pederast, Walter? I just want to say, sir, that we're both enormous-on a personal level.",
+        'featured': false,
+        'product_type': "product",
+        'id': '00003',
+        'discount': 0,
+        'img': 'images/rug-3.jpg'
+    };
+
+var product4 = {
+        'name': "The White Russian",
+        'price': 289.99,
+        'desc': "I just want to say, sir, that we're both enormous-on a personal level.",
+        'featured': false,
+        'product_type': "product",
+        'id': '00004',
+        'discount': 0,
+        'img': 'images/rug-4.jpg'
+    };
+
+var products  = [product1, product2, product3, product4];
+
+function testrun() {
+    for (var i = 0; i < products.length; i++) {
+        console.log(products[i].name);
+
+    }
+}
+
+//testrun();
+
+
+//
+//function buildProductHTML(e) {
+//    html =
+//}
+
+//
+//
+//
+//
 $.each(products, function(index, product) {
-    var productDiv = document.createElement( "div" );
-    productDiv.prop({
-        'class': product.type,
-        'id': product.id,
-        'data-name': product.name,
-        'data-price': product.price,
-    });
+    console.log(index, product.name);
+    var productDiv = document.createElement("div");
+    productDiv.id = product.id;
+    $(productDiv).addClass('product');
+    if (product.featured) {
+        $(productDiv).addClass('featured');
+    }
+    $(productDiv).data({'name': product.name, 'price': product.price });
+    productDiv.innerHTML = '<h1>Hi</h1> whats up';
+    document.getElementById('products').appendChild(productDiv);
+
+
+
+
+
+
+
+
+//    productDiv.appendTo('#products');
+});
+
+console.log('a' + )
+//
+//
+//		<div class="product featured" id="box" data-name="Dude's Rug" data-price="339.99">
+//            <img src='images/rug-1.jpg' class="productImage" />
+//            <h1 class="productName">Dude's Rug</h1>
+//            <h3 class="price">$339.99</h3>
+//            <h3 class="quantity"></h3>
+//            <button class="removeFromCart">Remove one from Cart</button>
+//			<button class="add">Add to cart</button>
+//			<button class="tellMore">Tell me more</button>
+//			<div class="moreInfo">
+//                <p>Hey! This is a private residence, man! Yeah man. Well, you know, the Dude abides. Hardly Dude, a new 'vette? The kid's still got, oh, 96 to 97 thousand, depending on the options. The Dude abides. Forget it, Donny. You're out of your element. Our basic freedoms. This is quite a pad you got here, man. Completely unspoiled. What's a pederast, Walter? I just want to say, sir, that we're both enormous-on a personal level.</p>
+//			</div>
+//		</div>
+
+
+
+
+
+
+
+
+
+
+////    console.log(product);
+//
+////    productDiv.appendTo('#products').prop( id, (product.id));
+//    $('#products').append(productDiv).prop(id, product.id);
+////    productDiv.prop({
+////        'class': product.product_type,
+//        'id': product.id,
+//        'data-name': product.name,
+//        'data-price': product.price
+//    });
 //    if (product.featured) {
 //        productDiv.addClass('featured');
 //    }
-    $('#products').append(productDiv);
-//
+
 //
 //    $('');
 
 //    var confirmYesButton = document.createElement('button');
 
 
-});
+//});
 
 
 //        $(this).parent().clone().appendTo('#cart').prop({ id: (cartId)});
--- products.js
+++ products.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
 
+
+
 var product1 = {
         'name': "Dude's Rug",
         'price': 339.99,
         'desc': "Hey! This is a private residence, man! Yeah man. Well, you know, the Dude abides. Hardly Dude, a new 'vette? The kid's still got, oh, 96 to 97 thousand, depending on the options. The Dude abides.",
         'featured': true,
-        'type': "product",
+        'product_type': "product",
         'discount': 15,
         'img': 'images/rug-1.jpg'
     };
@@ -16,7 +18,7 @@ var product2 = {
         'price': 249.99,
         'desc': "That, or Duder. His Dudeness. Or El Duderino, if you know, you're not into the whole brevity thing. We wanted to talk about little Larry. May we come in? DO YOU SEE WHAT HAPPENS, LARRY? They call Los Angeles the City of Angels. I didn't find it to be that exactly, but I'll allow as there are some nice folks there.",
         'featured': true,
-        'type': "product",
+        'product_type': "product",
         'discount': 10,
         'img': 'images/rug-2.jpg'
     };
@@ -26,7 +28,7 @@ var product3 = {
         'price': 289.99,
         'desc': "Forget it, Donny. You're out of your element. Our basic freedoms. This is quite a pad you got here, man. Completely unspoiled. What's a pederast, Walter? I just want to say, sir, that we're both enormous-on a personal level.",
         'featured': false,
-        'type': "product",
+        'product_type': "product",
         'discount': 0,
         'img': 'images/rug-3.jpg'
     };
@@ -36,7 +38,7 @@ var product4 = {
         'price': 289.99,
         'desc': "I just want to say, sir, that we're both enormous-on a personal level.",
         'featured': false,
-        'type': "product",
+        'product_type': "product",
         'discount': 0,
         'img': 'images/rug-4.jpg'
     };
